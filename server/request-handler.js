2030958b7b6ebe8d76c56218a6dd675f9e7fe314
Hack Reactor Students <accounts@hackreactor.com>
2014-11-17 16:57:49 -0800

    Hooked up app with new server.

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,7 +11,7 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-var messages = [];
+var messages = [{username: 'arthur', text: 'hi'}, {username: 'bernie', text: 'hi'}];
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -52,7 +52,7 @@ var requestHandler = function(request, response) {
       });
       request.on('end', function () {
         var post = JSON.parse(body);
-        console.log(post);
+        // console.log(post);
         messages.push(post);
       });
       statusCode = 201;
